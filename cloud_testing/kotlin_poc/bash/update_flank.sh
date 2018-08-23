ceda6d2c3d9eb2a366f19b826e04289cd24bddf3
bootstraponline <matt@bootstraponline.com>
2018-08-22 22:38:53 -0400

    Archive branch for Flank Java

--- cloud_testing/kotlin_poc/bash/update_flank.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-DIR=`dirname "$BASH_SOURCE"`
-
-FLANK="$DIR/.."
-
-"$FLANK/gradlew" -p "$FLANK" clean fatJar
-
-cp "$FLANK/build/libs/kotlin_poc-all-1.0-SNAPSHOT.jar" "$DIR/flank.jar"
--- cloud_testing/kotlin_poc/src/main/kotlin/ftl/json/MatrixMap.kt
+++
@@ -1,6 +0,0 @@
-package ftl.json
-
-data class MatrixMap(
-        val map: MutableMap<String, SavedMatrix>,
-        val runPath: String
-)
